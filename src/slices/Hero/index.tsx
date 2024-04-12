f9b52cb47ae245b5843052bbd1b475f95f42c9d1
Daniel <pepelacon@gmail.com>
2024-04-12 14:27:46 -0400

    hero section

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,38 @@
+import Bounded from "@/components/Bounded";
+import ButtonLink from "@/components/ButtonLink";
+import StarGrid from "@/components/StarGrid";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="relative">
+        <StarGrid />
+        {isFilled.richText(slice.primary.heading) && (
+          <PrismicRichText field={slice.primary.heading} />
+        )}
+        <PrismicRichText field={slice.primary.body} />
+        <ButtonLink field={slice.primary.button_link}>
+          {slice.primary.button_label}
+        </ButtonLink>
+
+        <PrismicNextImage field={slice.primary.image} />
+      </div>
+    </Bounded>
+  );
+};
+
+export default Hero;
